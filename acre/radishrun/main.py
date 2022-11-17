b6bb07b50b2907b43714b580a1a2d35264a56c84
Claudio Klingler <ck@realtime-projects.com>
2022-11-17 20:34:43 +0100

    manage artifacts and create result-xml. Closes #1.

--- acre/radishrun/main.py
+++ acre/radishrun/main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import subprocess
 import argparse
+import uuid
 import logging as log
 
 from acre.path import AcrePath
@@ -15,12 +16,17 @@ def main():
     parser.add_argument('--upgrade',
                         help="update dependencies according to the project's etc/requirements.txt",
                         action="store_true")
+    parser.add_argument('--trid', help="use the given trid (uuid4) as testrun-id",
+                        default=os.environ.get('ACRE_TRID', str(uuid.uuid4())))
     (myargs, options) = parser.parse_known_args()
 
     log.basicConfig(level=log.DEBUG)
     log.debug(f"arguments: {options}")
 
     userdata = _read_userdata()
+
+    artifacts = os.path.join("artifacts", myargs.trid)
+    os.makedirs(artifacts)
 
     if myargs.upgrade:
         cmd = "pip3 install --upgrade -r etc/requirements.txt"
@@ -29,8 +35,11 @@ def main():
             log.error("upgrading requirements failed")
             return 3
 
-    cmd = f'PYTHONPATH=src/ radish -t -b ./steps -b {AcrePath.steps()} {userdata} {" ".join(options)}'
-    log.info(f"running: {cmd}")
+    result_xml = os.path.join(artifacts, f"{myargs.trid}.xml")
+    env = f"-u TRID={myargs.trid} -u ARTIFACTS={artifacts}"
+    arguments = f"-t --bdd-xml {result_xml} {env} -b ./steps -b {AcrePath.steps()}"
+    cmd = f'PYTHONPATH=src/ radish {arguments}  {userdata} {" ".join(options)}'
+    log.info(f"running: {cmd} [{myargs.trid}]")
     os.environ['DISPLAY'] = ":99.0"
     return subprocess.run(cmd, shell=True).returncode
 
---
+++ releases.md
@@ -0,0 +1,7 @@
+# v0.1.0
+
+-   support creating artifacts
+
+# v0.0.1
+
+-   initial release of acre-radish
