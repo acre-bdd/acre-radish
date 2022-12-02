623ad4b9f87506b67f801ea0f698519a2bf39da4
Claudio Klingler <ck@realtime-projects.com>
2022-12-02 01:57:58 +0100

    don't redirect to logmon anymore.

--- acreradish/radishrun/main.py
+++ acreradish/radishrun/main.py
@@ -52,7 +52,7 @@ def main():
 
     env = f"-u TRID={myargs.trid} -u ARTIFACTS={artifacts}"
     arguments = f"-t --bdd-xml {result_xml} {env} -b ./steps -b {AcrePath.steps()}"
-    cmd = f'PYTHONPATH=src/ radish {arguments}  {userdata} {" ".join(options)} | tee /tmp/monitor2.log'
+    cmd = f'PYTHONPATH=src/ radish {arguments}  {userdata} {" ".join(options)}'
     log.trace(f"{cmd} [{myargs.trid}]")
     # monitor = open("monitor.log", "w")
     # radish = subprocess.Popen(cmd, shell=True, stdout=monitor, stderr=monitor)
